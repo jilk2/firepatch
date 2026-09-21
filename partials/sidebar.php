85f48a442b212d572791c47b341347a8a3451c55
Joah Quevedo Klein Haneveld <joahmqkh@gmail.com>
2026-09-21 11:34:20 +0200

    FIXED sidebar
    nu zie je de current page in blauw

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -10,12 +10,13 @@
 <body>
     <aside class="sidebar">
         <nav>
-            <a href="index.php" class="nav-item active"><span class="nav-icon">⌂</span>Overzicht</a>
-            <a href="mission.php" class="nav-item"><span class="nav-icon">●</span>Missies</a>
-            <a href="kaart.php" class="nav-item"><span class="nav-icon">🗺</span>Kaart</a>
-            <a href="logboek.php" class="nav-item"><span class="nav-icon">🗋</span>Logboek</a>
-            <a href="instellingen.php" class="nav-item"><span class="nav-icon">⚙</span>Instellingen</a>
-            <a href="verifynet.php" class="nav-item"><span class="nav-icon">⚙</span>VerifyNET</a>
+            <?php $currentpage = basename($_SERVER['PHP_SELF']); ?>
+            <a href="index.php" class="nav-item <?= $currentpage === 'index.php' ? ' current' : '' ?>"><span class="nav-icon">⌂</span>Overzicht</a>
+            <a href="mission.php" class="nav-item <?= $currentpage === 'mission.php' ? ' current' : '' ?>"><span class="nav-icon">●</span>Missies</a>
+            <a href="kaart.php" class="nav-item <?= $currentpage === 'kaart.php' ? ' current' : '' ?>"><span class="nav-icon">🗺</span>Kaart</a>
+            <a href="logboek.php" class="nav-item <?= $currentpage === 'logboek.php' ? ' current' : '' ?>"><span class="nav-icon">🗋</span>Logboek</a>
+            <a href="instellingen.php" class="nav-item <?= $currentpage === 'instellingen.php' ? ' current' : '' ?>"><span class="nav-icon">⚙</span>Instellingen</a>
+            <a href="verifynet.php" class="nav-item <?= $currentpage === 'verifynet.php' ? ' current' : '' ?>"><span class="nav-icon">⚙</span>VerifyNET</a>
         </nav>
         <div class="fleet">
             <h3>FLEET MATRIX</h3>
