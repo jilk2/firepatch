0bd7f79d63dbf0ca29b087f24db1390490d58aa0
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-09-23 10:48:40 +0200

    DBConnect toegevoegd + verifyNet table aangemaakt

---
+++ DB/DBConnect.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "FirePatch";
+
+// Create connection
+$db = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+?>
