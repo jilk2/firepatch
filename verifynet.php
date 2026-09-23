977b5b67881d211e4f47d8d0b6780f2ef0e60003
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-09-23 11:39:10 +0200

    added styling + records are being shown on the page

--- verifynet.php
+++ verifynet.php
@@ -26,41 +26,67 @@ mysqli_close($db);
     <div class="layout">
         <?php include("./partials/sidebar.php"); ?>
         <main class="page">
+            <section>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tijd</th>
+                            <th>Activiteit</th>
+                            <th>Locatie</th>
+                            <th>Afbeelding</th>
+                        </tr>
+                    </thead>
+                    <tfoot></tfoot>
+                    <tbody>
+                        <?php
+                        foreach ($claims as $claim):
+                            ?>
+                            <tr>
+                                <td>
+                                    <?= $claim['timestamp']; ?>
+                                </td>
+                                <td>
+                                    <?= $claim['Title']; ?>
+                                </td>
+                                <td>
+                                    <?= $claim['Locatie']; ?>
+                                </td>
+                            </tr>
+                            <?php
+                        endforeach;
+                        ?>
+                    </tbody>
+                </table>
+            </section>
         </main>
 
         <aside class="rightbar">
         </aside>
     </div>
-
-
-    <main>
-        <section>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Tijd</th>
-                        <th>Activiteit</th>
-                        <th>Locatie</th>
-                        <th>Afbeelding</th>
-                    </tr>
-                </thead>
-                <tfoot></tfoot>
-                <tbody>
-                    <?php
-                    foreach ($claims as $claim):
-                        ?>
-                        <tr>
-                            <td><?= $claim[''];?></td>
-                            <td><?= $claim[''];?></td>
-                            <td><?= $claim[''];?></td>
-                        </tr>
-                        <?php
-                    endforeach;
-                    ?>
-                </tbody>
-            </table>
-        </section>
-    </main>
 </body>
+<style>
+table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
+td, th, tr {
+  border: 1px solid black;
+  padding: 8px;
+  color: black
+}
+
+tr:nth-child(even){background-color: #f2f2f2;}
+
+tr:hover {background-color: #ddd;}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: black;
+}
+</style>
 </html>
